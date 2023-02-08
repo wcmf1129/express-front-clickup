6a9e38697457a768e6007f3a289829807e01a780
Alfred Wei <Alfred.Wei@realsteel.co.nz>
2023-02-08 13:47:46 +1300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    var ip = req.socket.remoteAddress;
+    console.log(ip,"Just got a request!")
+    res.send('Yo!',ip);
 })
 app.listen(process.env.PORT || 3000)
